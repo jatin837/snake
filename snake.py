3c0cdc4820d4b6c92075919c914396799e706c1f
Jatin Pandey <pandeyjatin47@gmail.com>
2020-07-27 23:07:50 +0530

    second commit
    trying to convert oop approach

--- snake.py
+++ snake.py
@@ -9,6 +9,14 @@ color = {
     'green': (0, 255, 0), 
     'blue' : (0, 0, 255), 
         }
+class Snake:
+    RADIUS = 10
+    BODY = [(display_width//2-2*Snake.RADIUS*i, display_height//2) for i in range(0, 4)]
+    def __init__():
+        pass
+    def draw(self, color, *args):
+        pygame.draw.circle(win, color['red'], (display_width//2, display_height//2), 10)
+
 pygame.draw.circle(win, color['red'], (display_width//2, display_height//2), 10)
 pygame.draw.circle(win, color['red'], (display_width//2 - 20, display_height//2), 10)
 pygame.draw.circle(win, color['red'], (display_width//2 - 20-20, display_height//2), 10)
