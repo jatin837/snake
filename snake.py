8759e3029a0c489c008f73fef8ed7803c6a3bd2d
Jatin Pandey <pandeyjatin47@gmail.com>
2020-07-29 13:37:09 +0530

    modified a bit

--- snake.py
+++ snake.py
@@ -11,34 +11,35 @@ color = {
  }
 class Snake:
     def __init__(self):
-        global display_height
-        global display_width
         self.RADIUS = 10
-        self.snakeBody = [ {'x':display_width//2-2*self.RADIUS*i ,'y': display_height//2, 'vel': 0} for i in range(0, 7)]
+        self.snakeBody = [ {'x':display_width//2-2*self.RADIUS*i ,'y': display_height//2, 'vx': 2*self.RADIUS, 'vy':0} for i in range(0, 7)]
     def draw(self):
         global win
-        for part in self.snakeBody:
+        for part in self.snakeBody:           
             pygame.draw.circle(win, (255, 0, 0), (part['x'], part['y']), self.RADIUS)
     def erase(self):
         global win
         for part in self.snakeBody:
             pygame.draw.circle(win, (0, 0, 0), (part['x'], part['y']), self.RADIUS)
-    def update_pos(self, vel):
-        for part in self.snakeBody:
-            part['vel'] = vel
-            part['x'] = part['x'] + part['vel']
+ 
 
-
+clock = pygame.time.Clock()
 snake = Snake()
 
 snake.draw()
-
+i = 0
 while not crashed:
+    clock.tick(2)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
     snake.erase()
-    snake.update_pos(1)
+    keys = pygame.key.get_pressed()
+    if keys['K_DOWN']:
+        i = len(snake.snakeBody)
+    for part in snake.snakeBody:
+        part['x'] = part['x'] + part['vx']
+        part['y'] = part['y'] + part['vy']
     snake.draw()
     pygame.display.update()
 pygame.quit()
