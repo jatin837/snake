bd1b9bf5f2e76865417d095344d09c5179344e89
Jatin Pandey <pandeyjatin47@gmail.com>
2020-07-30 18:40:09 +0530

    turning is resolved
    now desigin a way to delete turning points

--- snake.py
+++ snake.py
@@ -6,11 +6,25 @@ win = pygame.display.set_mode((display_width, display_height))
 crashed = False
 gridWidth = 25 
 convert = lambda j: j*gridWidth
-debugfile = open('debug.txt', 'w')
+
 rect = lambda x, y: pygame.Rect(x, y, gridWidth, gridWidth)
 class Snake:
     def __init__(self):
+        self.turningPoints = []
         self.snakeBody = [{'x':convert(5 - i),'y':convert(2), 'vx':convert(1), 'vy':convert(0)} for i in range(4)]
+    def changeDirection(self, part, direction):
+        if direction == 'D':
+            part['vx'] = convert(0)
+            part['vy'] = convert(1)
+        elif direction == 'R':
+            part['vx'] = convert(1)
+            part['vy'] = convert(0)
+        elif direction == 'U':
+            part['vx'] = convert(0)
+            part['vy'] = convert(-1)
+        elif direction == 'L':
+            part['vx'] = convert(-1)
+            part['vy'] = convert(0)
     def draw(self):
         global win
         for part in self.snakeBody:           
@@ -20,19 +34,22 @@ class Snake:
         for part in self.snakeBody:
             pygame.draw.rect(win, (0, 0, 0), rect(part['x'], part['y']))
     def update_pos(self):
-        dl = []
         for part in self.snakeBody:
+            for tp in self.turningPoints:
+                if part['x'] == tp['x'] and part['y'] == tp['y']:
+                    self.changeDirection(part, tp['direction'])
+                
             part['x'] += part['vx']
             part['y'] += part['vy']
-            dl.append(f'{part["x"]/gridWidth}, {part["y"]/gridWidth}')
-        print(dl, file = debugfile)
-	
 
-i = -1
+            
+
 clock = pygame.time.Clock()
 snake = Snake()
 snake.draw()
-turning = False
+
+
+i = len(snake.snakeBody) - 1
 while not crashed:
     clock.tick(10)
     # for i in range(1, display_height):
@@ -43,20 +60,16 @@ while not crashed:
             crashed = True
     snake.erase()
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_DOWN] and snake.snakeBody[0]['vy'] == 0:
+        snake.turningPoints.append({'x':snake.snakeBody[0]['x'], 'y': snake.snakeBody[0]['y'], 'direction':'D'})
+        
+    elif keys[pygame.K_LEFT] and snake.snakeBody[0]['vx'] == 0:
+        snake.turningPoints.append({'x':snake.snakeBody[0]['x'], 'y': snake.snakeBody[0]['y'], 'direction':'L'})
 
-    if keys[pygame.K_DOWN] and not turning:
-        print('turning starts', file = debugfile)
-        turning = True
-        i = len(snake.snakeBody) - 1
-
-    if turning:
-        print(f'turning {i}', file = debugfile)
-        if i >=0:
-            snake.snakeBody[len(snake.snakeBody) - 1 - i]['vx'] = convert(0)
-            snake.snakeBody[len(snake.snakeBody) - 1 - i]['vy'] = convert(1)
-            i -= 1
-        else:
-            turning = False
+    elif keys[pygame.K_UP] and snake.snakeBody[0]['vy'] == 0:
+        snake.turningPoints.append({'x':snake.snakeBody[0]['x'], 'y': snake.snakeBody[0]['y'], 'direction':'U'})
+    elif keys[pygame.K_RIGHT] and snake.snakeBody[0]['vx'] == 0:
+        snake.turningPoints.append({'x':snake.snakeBody[0]['x'], 'y': snake.snakeBody[0]['y'], 'direction':'R'})
     
     snake.update_pos()
     snake.draw()
