8b6c8305094ef7b86be74818e0db4802bf37eb18
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-17 13:48:28 +0530

    classify what data needed

--- snake.py
+++ snake.py
@@ -23,22 +23,20 @@ rect = lambda x, y: pygame.Rect(x, y, GRIDWIDTH, GRIDWIDTH)
 AVAILABLE:list = []
 clock = pygame.time.Clock()
 
-def _add_data():
+def _add_data(head_pos, food_pos, current_direction, length, next_direction, status):
     DATA_FILE:str = os.path.abspath('./snake.dat.json')
     with open(DATA_FILE, 'r') as f:
-        # adding dummy data to test
         data = json.load(f)
-        data["headPos"].append([9, 5])
-        data["foodPos"].append([1, 4])
-        data["length"].append(3)
-        data["direction"].append("Down")
-        data["status"].append("alive")
+        data["headPos"].append(head_pos)
+        data["foodPos"].append(food_pos)
+        data["length"].append(length)
+        data["current-direction"].append(current_direction)
+        data["next-direction"].append(next_direction)
+        data["status"].append(status)
 
     with open(DATA_FILE, 'w') as f:
-        json.dump(data, f, indent = 4)
+        json.dump(data, f, indent = 2)
 
-
-_add_data()
 class Body:
     def __init__(self, x, y, vx, vy):
         self.x = x
